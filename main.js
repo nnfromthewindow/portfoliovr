e8f2810232bbe49b790cec66c02d90500595e0f2
Nicolas Nuccelli <nuccelli@hotmail.com>
2023-08-31 19:40:49 -0300

    collisions added, working on skills logos

--- main.js
+++ main.js
@@ -1,108 +1,335 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+import { Octree } from 'three/addons/math/Octree.js';
+import { OctreeHelper } from 'three/addons/helpers/OctreeHelper.js';
+import { Capsule } from 'three/addons/math/Capsule.js';
 
 const loader = new GLTFLoader();
 
 function modelLoader(url) {
-    return new Promise((resolve, reject) => {
-      loader.load(url, data=> resolve(data), null, reject);
-    });
-  }
-
-
-
-  async function main() {
-
-	const clock = new THREE.Clock();
-
-    const canvas = document.querySelector( '#c' );
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
-	renderer.shadowMap.enabled = true;
-
-	const fov = 45;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 100;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.set( 4,2,-1);
-	camera.lookAt(-2,0,0)
-    
-	
-	const controls = new FirstPersonControls( camera, renderer.domElement );
-	controls.movementSpeed = 150;
-	controls.lookSpeed = 0.5;
-/*
-	const controls = new OrbitControls( camera, canvas );
-	controls.target.set( -2, 0, 0 );
-	controls.enableDamping = true;
-	controls.update();
-*/
-	const scene = new THREE.Scene();
-	scene.background = new THREE.Color( '#DEFEFF' );
-
-    const gltfData = await modelLoader('/assets/blender_test.gltf')
-
-    
-    const model = gltfData.scene;
-
-    gltfData.scene.children[3].intensity =50
-    
-	scene.add(model);
-	
-	console.log(gltfData)
-
-
-function resizeRendererToDisplaySize( renderer ) {
-
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if ( needResize ) {
-
-        renderer.setSize( width, height, false );
-
-    }
-
-    return needResize;
-
-}
-
-
-
-const mixer = new THREE.AnimationMixer( gltfData.scene );
-
-mixer.clipAction( gltfData.animations[ 1 ] ).play();
-
-	function render( time ) {
-
-		time *= 0.00001;
-
-        const delta = clock.getDelta();
-		if ( resizeRendererToDisplaySize( renderer ) ) {
-
-			const canvas = renderer.domElement;
-			camera.aspect = canvas.clientWidth / canvas.clientHeight;
-			camera.updateProjectionMatrix();
-
-		}
-
-     
-		renderer.render( scene, camera );
-       // controls.update();
-	   controls.update( clock.getDelta() );
-		mixer.update(delta)
-		requestAnimationFrame( render );
-
+	return new Promise((resolve, reject) => {
+		loader.load(url, data=> resolve(data), null, reject);
+	});
 	}
 
-	requestAnimationFrame( render );
+const clock = new THREE.Clock();
 
-}
+const scene = new THREE.Scene();
+//scene.background = new THREE.Color( 0x88ccee );
+scene.fog = new THREE.Fog( 0x88ccee, 0, 50 );
 
-main().catch(error => {
-    console.error(error);
-  });
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.rotation.order = 'YXZ';
 
+
+
+const container = document.getElementById( 'container' );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+container.appendChild( renderer.domElement );
+
+
+const GRAVITY = 30;
+
+const STEPS_PER_FRAME = 5;
+
+
+const worldOctree = new Octree();
+
+const playerCollider = new Capsule( new THREE.Vector3( 0, 0.35, 0 ), new THREE.Vector3( 0, 1.8, 0 ), 0.35 );
+
+const playerVelocity = new THREE.Vector3();
+const playerDirection = new THREE.Vector3();
+
+let playerOnFloor = false;
+let mouseTime = 0;
+
+const keyStates = {};
+
+const vector1 = new THREE.Vector3();
+const vector2 = new THREE.Vector3();
+const vector3 = new THREE.Vector3();
+
+document.addEventListener( 'keydown', ( event ) => {
+
+	keyStates[ event.code ] = true;
+
+} );
+
+document.addEventListener( 'keyup', ( event ) => {
+
+	keyStates[ event.code ] = false;
+
+
+} );
+
+container.addEventListener( 'mousedown', () => {
+
+
+		document.body.requestPointerLock();
+
+		mouseTime = performance.now();		
+
+} );
+
+
+
+
+/*
+			document.addEventListener( 'mouseup', () => {
+
+				if ( document.pointerLockElement !== null ) throwBall();
+
+			} );
+*/
+			document.body.addEventListener( 'mousemove', ( event ) => {
+
+				if ( document.pointerLockElement === document.body ) {
+
+					camera.rotation.y -= event.movementX / 500;
+					camera.rotation.x -= event.movementY / 500;
+
+				}
+
+				//if(document.pointerLockElement == null) console.log("hola")
+
+			} );
+
+		
+
+			window.addEventListener( 'resize', onWindowResize );
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+/*
+			function throwBall() {
+
+				const sphere = spheres[ sphereIdx ];
+
+				camera.getWorldDirection( playerDirection );
+
+				sphere.collider.center.copy( playerCollider.end ).addScaledVector( playerDirection, playerCollider.radius * 1.5 );
+
+				// throw the ball with more force if we hold the button longer, and if we move forward
+
+				const impulse = 15 + 30 * ( 1 - Math.exp( ( mouseTime - performance.now() ) * 0.001 ) );
+
+				sphere.velocity.copy( playerDirection ).multiplyScalar( impulse );
+				sphere.velocity.addScaledVector( playerVelocity, 2 );
+
+				sphereIdx = ( sphereIdx + 1 ) % spheres.length;
+
+			}
+*/ 
+			function playerCollisions() {
+
+				const result = worldOctree.capsuleIntersect( playerCollider );
+
+				playerOnFloor = false;
+
+				if ( result ) {
+
+					playerOnFloor = result.normal.y > 0;
+
+					if ( ! playerOnFloor ) {
+
+						playerVelocity.addScaledVector( result.normal, - result.normal.dot( playerVelocity ) );
+
+					}
+
+					playerCollider.translate( result.normal.multiplyScalar( result.depth ) );
+
+				}
+
+			}
+
+			function updatePlayer( deltaTime ) {
+
+				let damping = Math.exp( - 4 * deltaTime ) - 1;
+
+				if ( ! playerOnFloor ) {
+
+					playerVelocity.y -= GRAVITY * deltaTime;
+
+					// small air resistance
+					damping *= 0.1;
+
+				}
+
+				playerVelocity.addScaledVector( playerVelocity, damping );
+
+				const deltaPosition = playerVelocity.clone().multiplyScalar( deltaTime );
+				playerCollider.translate( deltaPosition );
+
+				playerCollisions();
+
+				camera.position.copy( playerCollider.end );
+
+			}
+
+
+
+			function getForwardVector() {
+
+				camera.getWorldDirection( playerDirection );
+				playerDirection.y = 0;
+				playerDirection.normalize();
+
+				return playerDirection;
+
+			}
+
+			function getSideVector() {
+
+				camera.getWorldDirection( playerDirection );
+				playerDirection.y = 0;
+				playerDirection.normalize();
+				playerDirection.cross( camera.up );
+
+				return playerDirection;
+
+			}
+
+			function controls( deltaTime ) {
+
+				// gives a bit of air control
+				const speedDelta = deltaTime * ( playerOnFloor ? 25 : 8 );
+
+				if ( keyStates[ 'KeyW' ] ) {
+
+					playerVelocity.add( getForwardVector().multiplyScalar( speedDelta ) );
+
+				}
+
+				if ( keyStates[ 'KeyS' ] ) {
+
+					playerVelocity.add( getForwardVector().multiplyScalar( - speedDelta ) );
+
+				}
+
+				if ( keyStates[ 'KeyA' ] ) {
+
+					playerVelocity.add( getSideVector().multiplyScalar( - speedDelta ) );
+
+				}
+
+				if ( keyStates[ 'KeyD' ] ) {
+
+					playerVelocity.add( getSideVector().multiplyScalar( speedDelta ) );
+
+				}
+
+
+				if ( playerOnFloor ) {
+
+					if ( keyStates[ 'Space' ] ) {
+
+						playerVelocity.y = 10;
+
+					}
+
+				}
+
+			}
+
+			const gltfData = await modelLoader('/assets/blender_test.gltf')
+
+			
+			const model = gltfData.scene;
+
+			model.children[4].intensity =50
+			
+			scene.add(model);
+			
+			console.log(gltfData)
+
+			
+
+			worldOctree.fromGraphNode(model);
+
+			model.traverse( child => {
+
+				if ( child.isMesh ) {
+
+					child.castShadow = true;
+					child.receiveShadow = true;
+
+					if ( child.material.map ) {
+
+						//child.material.map.anisotropy = 4;
+
+					}
+
+				}
+
+			} );
+
+			const helper = new OctreeHelper( worldOctree );
+			helper.visible = false;
+			scene.add( helper );
+
+			
+				
+
+
+			function teleportPlayerIfOob() {
+
+				if ( camera.position.y <= - 25 ) {
+
+					playerCollider.start.set( 0, 0.35, 0 );
+					playerCollider.end.set( 0, 1, 0 );
+					playerCollider.radius = 0.35;
+					camera.position.copy( playerCollider.end );
+					camera.rotation.set( 0, 0, 0 );
+
+				}
+
+			}
+
+
+
+
+			const mixer = new THREE.AnimationMixer( gltfData.scene );
+
+			mixer.clipAction( gltfData.animations[ 1 ] ).play();
+
+
+			function animate() {
+
+				const deltaTime = Math.min( 0.05, clock.getDelta() ) / STEPS_PER_FRAME;
+
+				// we look for collisions in substeps to mitigate the risk of
+				// an object traversing another too quickly for detection.
+
+				for ( let i = 0; i < STEPS_PER_FRAME; i ++ ) {
+
+					controls( deltaTime );
+
+					updatePlayer( deltaTime );
+
+
+					teleportPlayerIfOob();
+
+				}
+
+				mixer.update(deltaTime)
+
+				renderer.render( scene, camera );
+
+
+				requestAnimationFrame( animate );
+
+			}
+
+			animate();
